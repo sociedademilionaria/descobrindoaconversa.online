e72bfc9a932f54ab58b9f1276cf41bb421ce846f
Sociedade Milionaria <103861874+sociedademilionaria@users.noreply.github.com>
2022-07-12 19:42:13 -0300

    Add files via upload

---
+++ scripts/btn.js
@@ -0,0 +1,14 @@
+function perfectLink() {
+    var path = "https://descobrindoaconversa.online/";
+/*
+    var path = location.pathname+location.search;
+    var perfectlink  = '../' + path;
+    var perfectlink  = 'https://descobrindoaconversa.online/' + path;
+    console.log(location.pathname);
+    if (location.pathname == '/') {
+        var perfectlink  = 'ps';
+    } 
+    return location.href = perfectlink;
+*/
+    return location.href = path;
+}
